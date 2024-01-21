a4e7aa3a25c79082cda6c20c32c8cb28e274bd70
shaym <shaymaa2012@gmail.com>
2024-01-21 13:30:30 -0500

    Initial Commit

---
+++ src/test/java/Utilities/DriverClass.java
@@ -0,0 +1,67 @@
+package Utilities;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.safari.SafariDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Optional;
+import org.testng.annotations.Parameters;
+
+import java.io.IOException;
+import java.time.Duration;
+
+public class DriverClass {
+
+    public static WebDriver driver;
+    public WebDriverWait wait;
+
+    @BeforeClass(alwaysRun = true)
+    @Parameters("browserName")
+    public void createDriver(@Optional("chrome") String browser) {
+
+        switch (browser.toLowerCase()) {
+            case "chrome":
+                driver = new ChromeDriver();
+                break;
+            case "firefox":
+                driver = new FirefoxDriver();
+                break;
+            case "safari":
+                driver = new SafariDriver();
+                break;
+            case "edge":
+                driver = new EdgeDriver();
+                break;
+        }
+
+        wait = new WebDriverWait(driver, Duration.ofSeconds(15));
+
+
+        driver.manage().window().maximize();
+
+    }
+
+    @AfterClass(alwaysRun = true)
+    public void quitDriver() {
+        driver.quit();
+        closePreviousDrivers();
+    }
+
+
+
+    public void closePreviousDrivers() {
+        try {
+            Runtime.getRuntime().exec("taskkill /F /IM chromedriver.exe /T");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+}
