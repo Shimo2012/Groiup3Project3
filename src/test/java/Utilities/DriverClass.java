58aaacb00d2d38aeb7c1a78a9bc907285a24dfe0
shaym <shaymaa2012@gmail.com>
2024-01-21 20:58:41 -0500

    Actions are added

--- src/test/java/Utilities/DriverClass.java
+++ src/test/java/Utilities/DriverClass.java
@@ -1,11 +1,11 @@
 package Utilities;
 
-import org.openqa.selenium.By;
+import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.safari.SafariDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterClass;
@@ -17,15 +17,15 @@ import java.io.IOException;
 import java.time.Duration;
 
 public class DriverClass {
-
     public static WebDriver driver;
-    public WebDriverWait wait;
+    public static WebDriverWait wait;
+    public static Actions actions;
 
     @BeforeClass(alwaysRun = true)
     @Parameters("browserName")
-    public void createDriver(@Optional("chrome") String browser) {
-
+    public void createDriver(@Optional("chrome") String browser){
         switch (browser.toLowerCase()) {
+
             case "chrome":
                 driver = new ChromeDriver();
                 break;
@@ -40,28 +40,29 @@ public class DriverClass {
                 break;
         }
 
-        wait = new WebDriverWait(driver, Duration.ofSeconds(15));
-
-
         driver.manage().window().maximize();
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(15));
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+        actions = new Actions(driver);
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
 
     }
 
     @AfterClass(alwaysRun = true)
-    public void quitDriver() {
+    public void quitDriver(){
         driver.quit();
         closePreviousDrivers();
     }
 
-
-
-    public void closePreviousDrivers() {
+    public void closePreviousDrivers(){
         try {
-            Runtime.getRuntime().exec("taskkill /F /IM chromedriver.exe /T");
+            if (Platform.getCurrent().is(Platform.MAC)) {
+                Runtime.getRuntime().exec("pkill -f chromedriver");
+            } else if (Platform.getCurrent().is(Platform.WINDOWS)) {
+                Runtime.getRuntime().exec("taskkill /F /IM chromedriver.exe /T");
+            }
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
-
-
 }
