ff32fc45dccd9d63ce450d8668c5ffcf56cbc1a9
shaym <shaymaa2012@gmail.com>
2024-01-24 12:01:06 -0500

    Adding to cart and contact us are done

---
+++ src/test/java/Test_cases/_00_AllTestCases.java
@@ -0,0 +1,6 @@
+package Test_cases;
+
+import Utilities.DriverClass;
+
+public class _00_AllTestCases extends DriverClass {
+}
--- src/test/java/Test_cases/_01_US301.java
+++ src/test/java/Test_cases/_01_US301.java
@@ -1,11 +1,45 @@
 package Test_cases;
 
 import Utilities.DriverClass;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
 
 public class _01_US301 extends DriverClass {
 
-    public static void main(String[] args) {
-        System.out.println("Hi");
+    @Test
+    void addingEbookToBasket() {
+
+        String targetURL = ("https://shopdemo.e-junkie.com/");
+        String actualURL = driver.getCurrentUrl();
+        Assert.assertEquals(actualURL, targetURL, "URL is not correct");
+
+        actions.click(driver.findElement(By.cssSelector("button[onclick=\"return EJProductClick('1595015')\"]"))).perform();
+
+        driver.switchTo().frame(driver.findElement(By.cssSelector("iFrame[class=\"EJIframeV3 EJOverlayV3\"]")));
+
+        Assert.assertTrue(driver.findElement(By.cssSelector("div[class=\"Product-Image\"]")).isDisplayed());
+
+        actions.click(driver.findElement(By.xpath("//button[text()=\"Add Promo Code\"]"))).perform();
+
+        actions.click(driver.findElement(By.cssSelector("input[class=\"Promo-Code-Value\"]"))).sendKeys("0000").perform();
+
+        actions.click(driver.findElement(By.cssSelector("button[class=\"Promo-Apply\"]"))).perform();
+
+       wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("div[class=\"Overlay\"]")));
+
+      //  WebElement inValidPromoCode = driver.findElement(By.cssSelector("div[class=\"Overlay\"]"));
+
+     //  Assert.assertEquals(inValidPromoCode.getText(), "Invalid promo code.");
+
+      //  System.out.println("Test Passed: Invalid Promo code Message is displayed");
+
+
     }
 
 }
