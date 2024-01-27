b7c657bdff25f0beb46d51a0344edadfd41b6942
oksunlu <oksunluduygu@gmail.com>
2024-01-26 22:01:37 -0600

    new classes

--- src/test/java/Test_cases/_07_US307.java
+++ src/test/java/Test_cases/_07_US307.java
@@ -1,4 +1,20 @@
 package Test_cases;
 
-public class _07_US307 {
+import Utilities.DriverClass;
+import org.openqa.selenium.By;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class _07_US307 extends DriverClass {
+    //As a customer, I would like to access the e-junkie homepage from Shopdemo.e-Junkie.com  page.
+    // So I can quickly learn about this e-commerce site.
+
+    @Test
+    void homepage(){
+        driver.get("https://shopdemo.e-junkie.com/");
+
+        actions.click(driver.findElement(By.className("EJ-ShopLink"))).perform();
+
+        Assert.assertEquals(driver.getCurrentUrl(),"https://www.e-junkie.com/","URL");
+    }
 }
--- src/test/java/Test_cases/_08_US308.java
+++ src/test/java/Test_cases/_08_US308.java
@@ -1,4 +1,37 @@
 package Test_cases;
 
-public class _08_US308 {
+import Utilities.DriverClass;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.Test;
+
+import java.awt.*;
+import java.awt.event.KeyEvent;
+
+public class _08_US308 extends DriverClass {
+
+    //As a customer, I want to access a video that I can learn from the e-junkie homepage. So I can quickly learn about this e-commerce site.
+    @Test
+    public void test8() throws AWTException {
+
+        driver.get("https://shopdemo.e-junkie.com/");
+
+        actions.click(driver.findElement(By.className("EJ-ShopLink"))).perform();
+        WebElement seeHowItWorksBtn = driver.findElement(By.cssSelector("div[class=\"banner_btn\"] a"));
+        seeHowItWorksBtn.click();
+
+        Robot robot = new Robot();
+
+        for (int i = 0; i < 15; i++) {
+            robot.keyPress(KeyEvent.VK_TAB);
+            robot.keyRelease(KeyEvent.VK_TAB);
+        }
+
+
+        robot.keyPress(KeyEvent.VK_ENTER);
+        robot.keyRelease(KeyEvent.VK_ENTER);
+
+
+        quitDriver();
+    }
 }
